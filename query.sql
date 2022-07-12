ad5f4e34420f22f77726888fc8b1c77b2cbd5248
Dhea Amalia Lutfiani <74751990+damalialutfiani@users.noreply.github.com>
2022-07-12 13:54:21 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -9,3 +9,15 @@ ORDER BY city
 SELECT DISTINCT UPPER(city)
 FROM `bigquery-public-data.iowa_liquor_sales.sales` 
 ORDER BY 1
+
+#Second query to to delete the same value with different writing
+
+##to look the data before cleaning
+SELECT DISTINCT category
+FROM `bigquery-public-data.iowa_liquor_sales.sales` 
+ORDER BY 1
+
+##to remove ".0" in the category column
+SELECT DISTINCT REPLACE(category, ".0", "")
+FROM `bigquery-public-data.iowa_liquor_sales.sales` 
+ORDER BY 1
